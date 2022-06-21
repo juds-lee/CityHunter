9f098dd1ca5de7cd7bba49614e5a2210997545d0
juds-lee <judyjungmin.lee@gmail.com>
2022-06-21 16:42:12 -0400

    weather forecast

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const apiKey =  "3d91ba5b3c11d13158a2726aab902a0b";
 
   const [city, setCity] = useState("");
-  const [cityCordinates, setCityCordinates] = useState("");
   const handleCitySearch = (e) => {
     e.preventDefault(); 
      setCity(e.target[0].value);
@@ -18,20 +16,46 @@ function App() {
       method: "get",
       url: `https://api.openweathermap.org/geo/1.0/direct`,
       params: {
-        apikey: "3d91ba5b3c11d13158a2726aab902a0b",
+        appid: "3d91ba5b3c11d13158a2726aab902a0b",
         q: city,
       },
     };
    axios(cityLatLon)
       .then((response) => {
-        const cityLat = response.data[0].lat;
-        const cityLon =  response.data[0].lon;
-        console.log(cityLat, cityLon)
+        const results = response.data[0];
+        cityCoordinates(results)
       })
       .catch(function (error) {
         console.log(error);
       });
- });
+
+    const cityCoordinates = (result) => {
+      let latitude = result.lat
+      let longitude = result.lon
+      console.log(latitude, longitude)
+      weatherAppInfo(latitude, longitude)
+    };
+
+   const weatherAppInfo = (latitude, longitude) => {
+    const callWeatherApp = {
+      method: "get",
+        url: "https://api.openweathermap.org/data/2.5/weather",
+          params: {
+            appid: "3d91ba5b3c11d13158a2726aab902a0b",
+            lat: latitude,
+            lon: longitude
+        }
+    }
+    axios(callWeatherApp)
+        .then((response) => {
+          const results = response.data.main;
+          console.log(results)
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      }
+},[city]);
  
  
   return (
